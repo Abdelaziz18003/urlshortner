9056f2b7126fc906d4ea75d8c4457bd41028c1b9
Abdelaziz18003 <m.abdelaziz1993@gmail.com>
2017-03-24 22:10:13 +0100

    write redirecting to the original url router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var assert = require("assert");
+var mongo = require("mongodb").MongoClient;
+var dbUri = "mongodb://localhost:27017/url-shortner";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+/* GET the shortened URL. */
+router.get('/:shortUrl', function(req, res, next) {
+
+    // connect to the database
+    mongo.connect(dbUri, function(err, db) {
+        assert.equal(null, err, "can not connect to the database");
+
+        var urls = db.collection("urls");
+        var resultArray = [];
+
+        // search for the unique document containing the provided short url
+        var cursor = urls.find({ shortUrl: req.params.shortUrl });
+        cursor.forEach(function(doc) {
+            resultArray.push(doc);
+        }, function() {
+            res.redirect(resultArray[0].longUrl);
+        })
+    })
+});
+
 
 module.exports = router;
